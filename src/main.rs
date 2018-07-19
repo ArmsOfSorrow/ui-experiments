de453611a7c310958d434c1639438257fe61b1a1
ArmsOfSorrow <wave.dev@hotmail.com>
2018-07-19 23:17:23 +0200

    Add button

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 extern crate iui;
 
 use iui::UI;
-use iui::controls::{Window, WindowType};
+use iui::controls::{Window, WindowType, Button};
 
 fn main() {
     let ui = UI::init().unwrap();
-    let mut window = iui::controls::Window::new(
+    let mut window = Window::new(
         &ui,
         "hello window!",
         800,
         600,
         WindowType::HasMenubar
     );
+
+    let button = Button::new(
+        &ui,
+        "click me"
+    );
+
+    window.set_child(&ui, button);
     window.show(&ui);
     ui.main();
 }
